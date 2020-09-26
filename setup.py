9c64c87690bde7306192c78d2ed24428f072c91d
EvanSabre <evansabre@hotmail.fr>
2020-09-26 10:42:33 +0200

    Version 1.0 avec .exe dans dist

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
+import sys
 from cx_Freeze import setup, Executable
 
+pack = ["os",
+        "sys",
+        "wx",
+        "speech",
+        "queue",
+        "serial",
+        "time",
+        "codecs",
+        "random"]
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": pack, "excludes": ["tkinter","wx.lib.pdfviewer","numpy"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
 base = None
-executables = [Executable("src/main.py")]
-#Renseignez ici la liste complète des packages utilisés par votre application
-packages = ['idna', 'wx','os']
-options = {
-    'build_exe': {    
-        'packages':packages,
-    },
-}
-#Adaptez les valeurs des variables "name", "version", "description" à votre programme.
-setup(
-    name = "BlindIDE",
-    options = options,
-    version = "1.0",
-    description = 'Voici mon programme',
-    executables = executables
-)
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "Blind-IDE",
+        version = "1.0",
+        description = "IDE Micro Python",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("./src/main.py", base=base)])
